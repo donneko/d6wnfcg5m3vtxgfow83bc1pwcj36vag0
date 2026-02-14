bfb430c97fb9751e8ec0f43278074c56b4914c31
丼猫 <117331095+donneko@users.noreply.github.com>
2026-02-14 11:40:56 +0900

    main.js を更新
    クラス名変更

--- src/script/process-controller/main.js
+++ src/script/process-controller/main.js
@@ -1,6 +1,6 @@
 import {ARTICLE_DATA} from"./data/index.js";
 import {commandConverter} from "./util/command-converter.js";
-import {HtmlElementChanger} from "./util/html-element-changer.js";
+import {HtmlElementController} from "./util/html-element-changer.js";
 import {DataStoreController} from "./util/data-store-controller.js";
 import {addCssClass} from "./util/add-css-class.js";
 
@@ -16,13 +16,13 @@ class ProcessSectionController{
         this.#initEvent();
     }
     #initElement(){
-        this.EL_TAG_BOX_BUTTONS     = new HtmlElementChanger(".js-tag-box-buttons")
-        this.EL_BACKGROUND_MEDIA    = new HtmlElementChanger(".js-background-media")
-        this.EL_CONTENT_TITLE       = new HtmlElementChanger(".js-content-title")
-        this.EL_CONTENT_TEXT        = new HtmlElementChanger(".js-content-text")
-        this.EL_CONTENT_BACK_BUTTON = new HtmlElementChanger(".js-control-back-button")
-        this.EL_CONTENT_EACH_BUTTON = new HtmlElementChanger(".js-control-each-button")
-        this.EL_CONTENT_NEXT_BUTTON = new HtmlElementChanger(".js-control-next-button")
+        this.EL_TAG_BOX_BUTTONS     = new HtmlElementController(".js-tag-box-buttons")
+        this.EL_BACKGROUND_MEDIA    = new HtmlElementController(".js-background-media")
+        this.EL_CONTENT_TITLE       = new HtmlElementController(".js-content-title")
+        this.EL_CONTENT_TEXT        = new HtmlElementController(".js-content-text")
+        this.EL_CONTENT_BACK_BUTTON = new HtmlElementController(".js-control-back-button")
+        this.EL_CONTENT_EACH_BUTTON = new HtmlElementController(".js-control-each-button")
+        this.EL_CONTENT_NEXT_BUTTON = new HtmlElementController(".js-control-next-button")
     }
     #initDataSet(){
         this.DATA_STORE = new DataStoreController(ARTICLE_DATA);
